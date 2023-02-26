39ebf0f202b39ee4e2d7623829defa6e1ee00e74
Alexander Burachkov <zicio@yandex.ru>
2023-02-26 18:08:01 +0400

    Fixed

--- secondTask.js
+++ secondTask.js
@@ -15,5 +15,3 @@ export default class Calculator {
     return (BigInt(a) / BigInt(b)).toString();
   }
 }
-
-console.log(Calculator.add(89476546456834756834456534456847645473446753688546846465748768876768794955456679568797567889, 2));
